f8480afd09fb3139ad5b1d346af6189ad5ef19cf
Glen Carpenter <glen@glencarpenter.io>
2018-07-06 10:19:27 -0600

    Update scripts.js
    hide navbar on scroll

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 /*!
  * parallax.js v1.5.0 (http://pixelcog.github.io/parallax.js/)
  * @copyright 2016 PixelCog, Inc.
