be5ec2bc3f14215b2cba8a058efdaf9b2d7e7d6b
Glen Carpenter <glen@glencarpenter.io>
2019-03-02 12:38:43 -0700

    Update MainController.js

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -66,7 +66,7 @@ app.controller('MainController', ['$scope', function($scope) {
         {
             title: 'Glen Carpenter: Front End Developer',
             appLink: 'http://glencarpenter.io',
-            description1: 'This webpage, you are visiting!',
+            description1: 'The webpage you are currently visiting!',
             description2: 'This website was custom built with jQuery, Bootstrap, and AngularJS.',
             gitHubLink: 'https://github.com/GlenCarpenter/glencarpenterio',
             gitHubDesc: 'github.com/GlenCarpenter/glencarpenterio',
