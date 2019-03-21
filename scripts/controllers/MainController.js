39a7c2cf93136d44685db5f25200634131401c4b
Glen Carpenter <glen@glencarpenter.io>
2019-03-21 12:07:54 -0600

    Add files via upload
    updates to projects

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -19,31 +19,22 @@ app.controller('MainController', ['$scope', function($scope) {
             backgroundColor: {'background-color': '#ffb868'}
         },
         {
+            title: 'Poetry Simulator',
+            appLink: 'https://glencarpenter.github.io/poetry-simulator/',
+            description1: 'Poetry Simulator uses the DataMuse API to find rhyming and related words to automatically generate a poem, based off of a seed word provided by the user.',
+            description2: 'This app was built with ReactJS and makes use of HTTP requests with axios.',
+            gitHubLink: 'https://github.com/GlenCarpenter/poetry-simulator',
+            gitHubDesc: 'github.com/GlenCarpenter/poetry-simulator',
+            backgroundColor: {'background-color': 'Dodgerblue'}
+        },
+        {
             title: '8bit Paint',
             appLink: 'https://easy-8bit-paint.herokuapp.com/index.html',
             description1: '8bit Paint allows the user to paint a 16x16 grid in a retro sprite style and can even download the image when complete!',
             description2: 'Based on my love for all things Nintendo!',
             gitHubLink: 'https://github.com/GlenCarpenter/8bit_paint',
             gitHubDesc: 'github.com/GlenCarpenter/8bit_paint',
-            backgroundColor: {'background-color': 'Dodgerblue'}
-        },
-        {
-            title: 'React Key Logger',
-            appLink: 'https://codepen.io/GlenTheEskimo/pen/XoNQXR',
-            description1: 'React Key Logger is an open source proof-of-concept to show how user behavior can be tracked and measured.',
-            description2: 'The question is, how useful is it to harvest ALL user keystrokes on your app?  Is it ethical to do so?  The key logger was fairly easy to create, however the privacy implications must be carefully considered prior to deployment.',
-            gitHubLink: 'https://github.com/GlenCarpenter/reactKeyLogger',
-            gitHubDesc: 'github.com/GlenCarpenter/reactKeyLogger',
             backgroundColor: {'background-color': 'Sienna'}
-        },
-        {
-            title: 'Team Search',
-            appLink: '#',
-            description1: 'Chrome extension to search multiple data repositories, reducing search time.  Later I streamlined the search tool using regular expressions, thereby eliminating the need to specify which system to search.',
-            description2: 'Tool is used by a global team for day to day workflows.',
-            gitHubLink: 'https://github.com/GlenCarpenter/teamSearch',
-            gitHubDesc: 'github.com/GlenCarpenter/teamSearch',
-            backgroundColor: {'background-color': 'SteelBlue'}
         },
         {
             title: 'Montessori Household',
@@ -52,7 +43,25 @@ app.controller('MainController', ['$scope', function($scope) {
             description2: 'The site was initially created as an market evaluation for the products being sold, which was then updated to a full fledged web store.',
             gitHubLink: '',
             gitHubDesc: '',
+            backgroundColor: {'background-color': 'SteelBlue'}
+        },
+        {
+            title: 'React Key Logger',
+            appLink: 'https://codepen.io/GlenTheEskimo/pen/XoNQXR',
+            description1: 'React Key Logger is an open source proof-of-concept to show how user behavior can be tracked and measured.',
+            description2: 'The question is, how useful is it to harvest ALL user keystrokes on your app?  Is it ethical to do so?  The key logger was fairly easy to create, however the privacy implications must be carefully considered prior to deployment.',
+            gitHubLink: 'https://github.com/GlenCarpenter/reactKeyLogger',
+            gitHubDesc: 'github.com/GlenCarpenter/reactKeyLogger',
             backgroundColor: {'background-color': '#ffb868'}
+        },
+        {
+            title: 'Team Search',
+            appLink: '#',
+            description1: 'Chrome extension to search multiple data repositories, reducing search time.  Later I streamlined the search tool using regular expressions, thereby eliminating the need to specify which system to search.',
+            description2: 'Tool is used by a global team for day to day workflows.',
+            gitHubLink: 'https://github.com/GlenCarpenter/teamSearch',
+            gitHubDesc: 'github.com/GlenCarpenter/teamSearch',
+            backgroundColor: {'background-color': 'Dodgerblue'}
         },
         {
             title: 'VetRelief Site Enhancement',
@@ -61,7 +70,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description2: 'This is a Chrome extension that changes layout of VetRelief.com website, which is owned and maintained by a third party developer.  This is accomplished by use of jQuery plugins and HTML/CSS injection.',
             gitHubLink: '',
             gitHubDesc: '',
-            backgroundColor: {'background-color': 'Dodgerblue'}
+            backgroundColor: {'background-color': 'Sienna'}
         },
         {
             title: 'Reddit User Search',
@@ -70,15 +79,6 @@ app.controller('MainController', ['$scope', function($scope) {
             description2: '',
             gitHubLink: 'http://github.com/GlenCarpenter/RedditUserApp',
             gitHubDesc: 'github.com/GlenCarpenter/RedditUserApp',
-            backgroundColor: {'background-color': 'Sienna'}
-        },
-        {
-            title: 'Poetry Simulator',
-            appLink: 'https://repl.it/@GlenCarpenter/Poetry-Simulator',
-            description1: 'Poetry Simulator uses the DataMuse API to find rhyming and related words to automatically generate a poem, based off of a seed word provided by the user.',
-            description2: '',
-            gitHubLink: '',
-            gitHubDesc: '',
             backgroundColor: {'background-color': 'SteelBlue'}
         },
         {
