cfffd7c7073b4f4cd9c3d91b96528d6b6dedee5e
Glen Carpenter <glen@glencarpenter.io>
2019-07-31 11:13:15 -0600

    Update MainController.js
    Updates to GitHub description to fix overflow on mobile

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -6,7 +6,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'New Saigon Grill is a Vietnamese restaurant in Denver, CO.',
             description2: 'The website uses ReactJS with React Router, react-scroll-parallax, and google-map-react to create a seamless and responsive single page application.  The site is easy to navigate and use on both desktop and mobile.',
             gitHubLink: 'https://github.com/GlenCarpenter/new-saigon-denver',
-            gitHubDesc: 'github.com/GlenCarpenter/new-saigon-denver',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'SteelBlue'}
         },
         {
@@ -15,7 +15,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'CoffeeLab Solutions is a software solutions and project management service provider.',
             description2: 'The website I created is a lead generation landing page designed to drive sales.    The site is easy to navigate and use on both desktop and mobile.',
             gitHubLink: 'https://github.com/GlenCarpenter/CoffeeLabSolutions',
-            gitHubDesc: 'github.com/GlenCarpenter/CoffeeLabSolutions',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': '#ffb868'}
         },
         {
@@ -24,7 +24,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'Poetry Simulator uses the DataMuse API to find rhyming and related words to automatically generate a poem, based off of a seed word provided by the user.',
             description2: 'This app was built with ReactJS and makes use of HTTP requests with axios.',
             gitHubLink: 'https://github.com/GlenCarpenter/poetry-simulator',
-            gitHubDesc: 'github.com/GlenCarpenter/poetry-simulator',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'Dodgerblue'}
         },
         {
@@ -33,7 +33,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: '8bit Paint allows the user to paint a 16x16 grid in a retro sprite style and can even download the image when complete!',
             description2: 'Based on my love for all things Nintendo!',
             gitHubLink: 'https://github.com/GlenCarpenter/8bit_paint',
-            gitHubDesc: 'github.com/GlenCarpenter/8bit_paint',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'Sienna'}
         },
         {
@@ -42,7 +42,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'RetroTube is an app that allows you to search for YouTube videos in a throwback 2007 aesthetic.  ',
             description2: 'The app uses ReactJS and axios for API calls.',
             gitHubLink: 'https://github.com/GlenCarpenter/retrotube',
-            gitHubDesc: 'github.com/GlenCarpenter/retrotube',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'SteelBlue'}
         },
         {
@@ -60,7 +60,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'React Key Logger is a proof-of-concept to show how user behavior can be tracked and measured by logging all user inputs.',
             description2: 'The question is, how useful is it to harvest ALL user keystrokes on your app?  Is it ethical to do so?  The key logger was fairly easy to create, however the privacy implications must be carefully considered prior to deployment.',
             gitHubLink: 'https://github.com/GlenCarpenter/reactKeyLogger',
-            gitHubDesc: 'github.com/GlenCarpenter/reactKeyLogger',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'Dodgerblue'}
         },
         {
@@ -69,7 +69,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'Chrome extension to search multiple data repositories, reducing search time.  Later I streamlined the search tool using regular expressions, thereby eliminating the need to specify which system to search.',
             description2: 'Tool is used by a global team for day to day workflows.',
             gitHubLink: 'https://github.com/GlenCarpenter/teamSearch',
-            gitHubDesc: 'github.com/GlenCarpenter/teamSearch',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'Sienna'}
         },
         {
@@ -87,7 +87,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'Reddit User Search is a Chrome extension that allows you to search a Reddit User\'s profile and posts with ease.',
             description2: '',
             gitHubLink: 'http://github.com/GlenCarpenter/RedditUserApp',
-            gitHubDesc: 'github.com/GlenCarpenter/RedditUserApp',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': '#ffb868'}
         },
         {
@@ -96,7 +96,7 @@ app.controller('MainController', ['$scope', function($scope) {
             description1: 'The webpage you are currently visiting!',
             description2: 'This website was custom built with jQuery, Bootstrap, and AngularJS.',
             gitHubLink: 'https://github.com/GlenCarpenter/glencarpenterio',
-            gitHubDesc: 'github.com/GlenCarpenter/glencarpenterio',
+            gitHubDesc: 'View on GitHub',
             backgroundColor: {'background-color': 'Dodgerblue'}
         }
     ];
